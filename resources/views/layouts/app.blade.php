9c5697fa11843298094947ffb74090bad2ba6870
hajjamn <hajjamn@gmail.com>
2025-01-09 20:28:47 +0100

    FIX: products image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/products') }}">{{ __('Home') }}</a>
+                            <a class="nav-link" href="{{ url('/admin/products') }}">{{ __('Prodotti') }}</a>
                         </li>
                     </ul>
 
